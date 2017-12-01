4709e45289664dab2746429c060cf6cf02ff5bed
mikeathers <athers_05@hotmail.co.uk>
2017-12-01 18:43:21 +0000

    Updated ExpenseListItem

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -3,10 +3,15 @@ import { Link } from "react-router-dom";
 import moment from "moment";
 import numeral from "numeral";
 
-const ExpenseListItem = ({ id, description, totalCost, date }) => (
+const ExpenseListItem = ({ id, description, totalCost, date, createdAt }) => (
   <Link className="list-item" to="">
-  <h3>{description}</h3>
+    <div>
+      <h3 className="list-item__title">{description}</h3>
+      <span className="list-item__sub-title">{moment(createdAt).format("MMMM Do, YYYY")}</span>
+    </div>
+    <h3 className="list-item__data">{"£" + numeral(totalCost).format("0,0.00")}</h3>
   </Link>
+  
 );
 
 export default ExpenseListItem;
